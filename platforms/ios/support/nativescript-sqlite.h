411c26e0f3f5859d5f9d19179455e670f39c0c3b
Nathanael Anderson <nathan@master-technology.com>
2015-10-21 21:53:44 -0500

    Another cocoamodule test

--- platforms/ios/support/nativescript-sqlite.h
+++ platforms/ios/support/nativescript-sqlite.h
@@ -1,11 +1,5 @@
 /**********************************************
   (c)2015, Master Technology
    This file is under the MIT license.
-   
-   It is only used as a dummy file because of
-   the inability of the Cocoapod spec to do 
-   something simple like create a pod without
-   source code.   Silly pod Developers, hacks
-   are stupid.   
 ***********************************************/
-   
+#import <sqlite3.h>   
